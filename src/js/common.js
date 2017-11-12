5b5256027361fd80dfeb794c7f8c87164cc8f71f
artlenk.ru@gmail.com <df5ukoi7ht>
2017-11-12 23:49:01 +0300

    Refactoring header

--- src/js/common.js
+++ src/js/common.js
@@ -4,16 +4,36 @@ $(document).ready(function () {
             $('.menu').toggleClass("menu-active");
         }
     );
-    $(".header-search").click(
-        function () {
-            $('.header-search').addClass("header-search-enabled");
-        }
-    );
 
-    $(function(){
-        $('#to-top-btn').on('click', function(e){
-            $('html,body').stop().animate({ scrollTop: $('body').offset().top }, 1000);
+    $(function () {
+        $('#to-top-btn').on('click', function (e) {
+            $('html,body').stop().animate({scrollTop: $('body').offset().top}, 1000);
             e.preventDefault();
         });
     });
+
+// BEGIN: Открытие и закрытие поисковой панели в шапке сайта
+    var searchTop = $('.search-top'),
+        searchTopClose = $('.search-top-close'),
+        searchTopOpened = 'search-top-opened';
+
+    $('.search-top-icon').on('click', function () {
+        if (searchTop.hasClass(searchTopOpened)) {
+            searchTop.removeClass(searchTopOpened);
+        } else {
+            searchTop.addClass(searchTopOpened);
+        }
+    });
+
+    searchTopClose.on('click', function () {
+        searchTop.removeClass(searchTopOpened);
+    });
+
+    $(document).click(function (event) {
+        if ($(event.target).closest('.search-top').length)
+            return;
+        searchTop.removeClass(searchTopOpened);
+        event.stopPropagation();
+    });
+// END
 });
